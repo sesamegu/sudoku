fc08ff41be7519978937e2db3217b5dffef6489b
haiquan.guhq <haiquan.guhq@alibaba-inc.com>
2022-10-25 22:11:12 +0800

    add Y wing box

--- src/main/java/com/sesame/game/strategy/YWingStrategy.java
+++ src/main/java/com/sesame/game/strategy/YWingStrategy.java
@@ -17,7 +17,8 @@ import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
 /**
- * Introduction:Y翼，有四种造型：右下、左下、右上、左上
+ * Introduction:Y翼，基于行列的有四种造型：右下、左下、右上、左上。基于宫的，有8个造型右下、左下、右上、左上、
+ * 右右下、左左下、右右上、左左上
  *
  * @author sesame 2022/10/23
  */
@@ -49,16 +50,167 @@ public class YWingStrategy implements FillStrategy {
             }
         }
 
-        if (allRowList.size() < 2 || allColumnList.size() < 2) {
-            return Optional.empty();
+        Optional<HintModel> result = processRowHintModel(remaining, allRowList, allColumnList);
+        if (result.isPresent()) {
+            return result;
         }
 
-        return processRowHintModel(remaining, allRowList);
+        //以宫为单位，遍历9个宫
+        for (int row = 0; row < Const.ROWS; row = row + Const.BOX_WIDTH) {
+            for (int column = 0; column < Const.COLUMNS; column = column + Const.BOX_WIDTH) {
+
+                //过滤出后续个数为2的位置列表
+                List<Position> boxList = PuzzleTools.getPositionByBox(row, column);
+                List<Position> filterList = boxList.stream().filter(one -> (!CollectionUtils.isEmpty(
+                    remaining.get(one))) && remaining.get(one).size() == 2).collect(Collectors.toList());
+                int size = filterList.size();
+                if (size < 2) {
+                    continue;
+                }
+
+                Optional<HintModel> hintModel = processBoxHintModel(remaining, filterList, row, column);
+                if (hintModel.isPresent()) {
+                    return hintModel;
+                }
+
+            }
+        }
+        return Optional.empty();
 
     }
 
+    private Optional<HintModel> processBoxHintModel(Map<Position, List<String>> remaining,
+        List<Position> filterList, int row, int column) {
+        int size = filterList.size();
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                Position first = filterList.get(i);
+                Position second = filterList.get(j);
+                //不在同行同列
+                if (first.getCol() == second.getCol() || first.getRow() == second.getRow()) {
+                    continue;
+                }
+
+                //两个候选数一个是相同，一个不同
+                List<String> sameDigital = new ArrayList<>(remaining.get(first));
+                sameDigital.retainAll(remaining.get(second));
+                if (sameDigital.size() != 1) {
+                    continue;
+                }
+
+                //找出第三格需要匹配的候选数组
+                List<String> thirdDigital = new ArrayList<>();
+                thirdDigital.addAll(remaining.get(first));
+                thirdDigital.addAll(remaining.get(second));
+                thirdDigital.removeAll(sameDigital);
+                Assert.isTrue(thirdDigital.size() == 2, "should be two");
+                Collections.sort(thirdDigital);
+
+                // 查找第一个数列：因为第1列和第2列位置不定，因此同样存在四种造型情况
+                List<String> tempDigital = new ArrayList<>(thirdDigital);
+                tempDigital.removeAll(remaining.get(first));
+                Assert.isTrue(tempDigital.size() == 1, "should be one");
+                String firstDeleteDigital = tempDigital.get(0);
+
+                List<Position> positionByColumn = PuzzleTools.getPositionByColumn(first.getCol());
+                Optional<HintModel> hintModel = boxBuildHintModel(remaining, row, column, first, second,
+                    thirdDigital, positionByColumn, firstDeleteDigital, Direction.COLUMN, second.getCol(), -1);
+                if (hintModel.isPresent()) {
+                    return hintModel;
+                }
+
+                // 查找第二数列：因为第1列和第2列位置不定，因此同样存在四种造型情况
+                tempDigital = new ArrayList<>(thirdDigital);
+                tempDigital.removeAll(remaining.get(second));
+                Assert.isTrue(tempDigital.size() == 1, "should be one");
+                String secondDeleteDigital = tempDigital.get(0);
+                positionByColumn = PuzzleTools.getPositionByColumn(second.getCol());
+                hintModel = boxBuildHintModel(remaining, row, column, first, second,
+                    thirdDigital, positionByColumn, secondDeleteDigital, Direction.COLUMN, first.getCol(), -1);
+                if (hintModel.isPresent()) {
+                    return hintModel;
+                }
+
+                //  查找第一个行数：因为第1行和第2行位置不定，因此同样存在四种造型情况
+                List<Position> positionByRow = PuzzleTools.getPositionByRow(first.getRow());
+                hintModel = boxBuildHintModel(remaining, row, column, first, second,
+                    thirdDigital, positionByRow, firstDeleteDigital, Direction.ROW, -1, second.getRow());
+                if (hintModel.isPresent()) {
+                    return hintModel;
+                }
+
+                //  查找第二个行数：因为第1行和第2行位置不定，因此同样存在四种造型情况
+                positionByRow = PuzzleTools.getPositionByRow(second.getRow());
+                hintModel = boxBuildHintModel(remaining, row, column, first, second,
+                    thirdDigital, positionByRow, firstDeleteDigital, Direction.ROW, -1, first.getRow());
+                if (hintModel.isPresent()) {
+                    return hintModel;
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<HintModel> boxBuildHintModel(Map<Position, List<String>> remaining, int row, int column,
+        Position first, Position second, List<String> thirdDigital, List<Position> positionList,
+        String deleteDigital, Direction direction, int forthColumn, int forthRow) {
+        for (Position third : positionList) {
+            //过滤同宫的位置
+            if (direction == Direction.COLUMN) {
+                int thirdRow = third.getRow();
+                if (thirdRow == row || thirdRow == row + 1 || thirdRow == row + 2) {
+                    continue;
+                }
+            } else {
+                int thirdColumn = third.getCol();
+                if (thirdColumn == column || thirdColumn == column + 1 || thirdColumn == column + 2) {
+                    continue;
+                }
+            }
+
+            if (CollectionUtils.isEmpty(remaining.get(third))) {
+                continue;
+            }
+
+            List<String> thirdRemain = new ArrayList<>(remaining.get(third));
+            if (!thirdRemain.equals(thirdDigital)) {
+                continue;
+            }
+
+            //找到第四位置的候选数中包含 删除数
+            Position forth;
+            if (direction == Direction.COLUMN) {
+                forth = new Position(third.getRow(), forthColumn);
+            } else {
+                forth = new Position(forthRow, third.getCol());
+            }
+            if (!CollectionUtils.isEmpty(remaining.get(forth)) && remaining.get(forth).contains(
+                deleteDigital)) {
+                Map<Position, List<String>> causeMap = new HashMap<>();
+                causeMap.put(first, new ArrayList<>(remaining.get(first)));
+                causeMap.put(second, new ArrayList<>(remaining.get(second)));
+                causeMap.put(third, new ArrayList<>(remaining.get(third)));
+
+                Map<Position, List<String>> deleteMap = new HashMap<>();
+                List<String> forthDigital = new ArrayList<>();
+                forthDigital.add(deleteDigital);
+                deleteMap.put(forth, forthDigital);
+                CandidateModel candidateModel = new CandidateModel(causeMap, deleteMap);
+                HintModel tt = HintModel.build().of(getStrategy()).of(candidateModel);
+                return Optional.of(tt);
+            }
+
+        }
+        return Optional.empty();
+    }
+
     private Optional<HintModel> processRowHintModel(Map<Position, List<String>> remaining,
-        List<List<Position>> allList) {
+        List<List<Position>> allList, List<List<Position>> allColumnList) {
+        if (allList.size() < 2 || allColumnList.size() < 2) {
+            return Optional.empty();
+        }
+
         // 对每行进行双循环遍历
         for (int i = 0; i < allList.size(); i++) {
             List<Position> positionList = allList.get(i);
@@ -68,41 +220,42 @@ public class YWingStrategy implements FillStrategy {
             for (int n = 0; n < positionList.size() - 1; n++) {
                 for (int m = n + 1; m < positionList.size(); m++) {
 
-                    Position front = positionList.get(n);
-                    Position behind = positionList.get(m);
+                    Position first = positionList.get(n);
+                    Position second = positionList.get(m);
                     //找出有共同一个数字的两个位置
-                    List<String> causeDigital = new ArrayList<>(remaining.get(front));
-                    causeDigital.retainAll(remaining.get(behind));
+                    List<String> causeDigital = new ArrayList<>(remaining.get(first));
+                    causeDigital.retainAll(remaining.get(second));
                     if (causeDigital.size() != 1) {
                         continue;
                     }
                     //找出第三格需要匹配的候选数组
                     List<String> thirdDigital = new ArrayList<>();
-                    thirdDigital.addAll(remaining.get(front));
-                    thirdDigital.addAll(remaining.get(behind));
+                    thirdDigital.addAll(remaining.get(first));
+                    thirdDigital.addAll(remaining.get(second));
                     thirdDigital.removeAll(causeDigital);
                     Assert.isTrue(thirdDigital.size() == 2, "should be two");
                     Collections.sort(thirdDigital);
 
                     // 右下、右上：查找前列，是否存在一个单元格剩余数符合
                     List<String> tempDigital = new ArrayList<>(thirdDigital);
-                    tempDigital.removeAll(remaining.get(front));
+                    tempDigital.removeAll(remaining.get(first));
                     Assert.isTrue(tempDigital.size() == 1, "should be one");
                     String deleteDigital = tempDigital.get(0);
-                    Optional<HintModel> result = buildHintModel(remaining, front, behind,
-                        thirdDigital, deleteDigital, front.getCol(), behind.getCol());
+
+                    Optional<HintModel> result = buildHintModel(remaining, first, second,
+                        thirdDigital, deleteDigital, first.getCol(), second.getCol());
                     if (result.isPresent()) {
                         return result;
                     }
 
                     tempDigital = new ArrayList<>(thirdDigital);
-                    tempDigital.removeAll(remaining.get(behind));
+                    tempDigital.removeAll(remaining.get(second));
                     Assert.isTrue(tempDigital.size() == 1, "should be one");
                     deleteDigital = tempDigital.get(0);
 
                     // 左下、左上：查找后列，是否存在一个单元格剩余数符合
-                    result = buildHintModel(remaining, front, behind,
-                        thirdDigital, deleteDigital, behind.getCol(), front.getCol());
+                    result = buildHintModel(remaining, first, second,
+                        thirdDigital, deleteDigital, second.getCol(), first.getCol());
                     if (result.isPresent()) {
                         return result;
                     }
--- src/test/java/com/sesame/game/strategy/YWingStrategyTest.java
+++ src/test/java/com/sesame/game/strategy/YWingStrategyTest.java
@@ -1,5 +1,6 @@
 package com.sesame.game.strategy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -195,5 +196,206 @@ public class YWingStrategyTest {
         Assert.assertEquals(1, delDigital.size());
         Assert.assertEquals("4", delDigital.get(0));
     }
+
+    @Test
+    public void test_box_right_up_first() {
+        String[][] board = new String[][] {
+            {"8", "9", "1", "3", "", "", "", "", "6"},
+            {"3", "", "7", "6", "2", "9", "", "8", "1"},
+            {"6", "", "2", "", "8", "1", "", "", "7"},
+            {"4", "7", "", "", "6", "", "", "1", "2"},
+            {"5", "3", "", "", "1", "2", "", "6", "4"},
+            {"1", "2", "6", "8", "", "4", "", "", "9"},
+            {"9", "1", "5", "2", "", "", "6", "", "8"},
+            {"7", "8", "3", "", "9", "6", "", "", "5"},
+            {"2", "6", "4", "", "5", "8", "", "", "3"}
+        };
+        SudokuPuzzle puzzle = new SudokuPuzzle();
+        puzzle.setBoard(board);
+        Optional<HintModel> result = new YWingStrategy().tryStrategy(puzzle);
+        Assert.assertTrue(result.isPresent());
+        HintModel hintModel = result.get();
+        Assert.assertTrue(hintModel.isCandidate());
+        CandidateModel candidateModel = hintModel.getCandidateModel();
+
+        Map<Position, List<String>> causeMap = candidateModel.getCauseMap();
+        Assert.assertEquals(3, causeMap.size());
+        List<String> strings = causeMap.get(new Position(3, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("3", strings.get(0));
+        Assert.assertEquals("5", strings.get(1));
+
+        strings = causeMap.get(new Position(5, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("3", strings.get(0));
+        Assert.assertEquals("7", strings.get(1));
+
+        strings = causeMap.get(new Position(0, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("5", strings.get(0));
+        Assert.assertEquals("7", strings.get(1));
+
+        Map<Position, List<String>> deleteMap = candidateModel.getDeleteMap();
+        Assert.assertEquals(1, deleteMap.size());
+        List<String> delDigital = deleteMap.get(new Position(0, 4));
+        Assert.assertEquals(1, delDigital.size());
+        Assert.assertEquals("7", delDigital.get(0));
+    }
+
+    @Test
+    public void test_box_right_down_first() {
+        String[][] board = new String[][] {
+            {"9", "1", "5", "2", "", "", "6", "", "8"},
+            {"7", "8", "3", "", "9", "6", "", "", "5"},
+            {"2", "6", "4", "", "5", "8", "", "", "3"},
+            {"4", "7", "", "", "6", "", "", "1", "2"},
+            {"5", "3", "", "", "1", "2", "", "6", "4"},
+            {"1", "2", "6", "8", "", "4", "", "", "9"},
+            {"8", "9", "1", "3", "", "", "", "", "6"},
+            {"3", "", "7", "6", "2", "9", "", "8", "1"},
+            {"6", "", "2", "", "8", "1", "", "", "7"},
+        };
+        SudokuPuzzle puzzle = new SudokuPuzzle();
+        puzzle.setBoard(board);
+        Optional<HintModel> result = new YWingStrategy().tryStrategy(puzzle);
+        Assert.assertTrue(result.isPresent());
+        HintModel hintModel = result.get();
+        Assert.assertTrue(hintModel.isCandidate());
+        CandidateModel candidateModel = hintModel.getCandidateModel();
+
+        Map<Position, List<String>> causeMap = candidateModel.getCauseMap();
+        Assert.assertEquals(3, causeMap.size());
+        List<String> strings = causeMap.get(new Position(3, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("3", strings.get(0));
+        Assert.assertEquals("5", strings.get(1));
+
+        strings = causeMap.get(new Position(5, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("3", strings.get(0));
+        Assert.assertEquals("7", strings.get(1));
+
+        strings = causeMap.get(new Position(6, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("5", strings.get(0));
+        Assert.assertEquals("7", strings.get(1));
+
+        Map<Position, List<String>> deleteMap = candidateModel.getDeleteMap();
+        Assert.assertEquals(1, deleteMap.size());
+        List<String> delDigital = deleteMap.get(new Position(6, 4));
+        Assert.assertEquals(1, delDigital.size());
+        Assert.assertEquals("7", delDigital.get(0));
+    }
+
+    @Test
+    public void test_box_left_up_first() {
+        String[][] board = new String[][] {
+            {"1", "8", "4", "", "", "3", "5", "6", "9"},
+            {"7", "3", "", "", "", "5", "", "8", "1"},
+            {"", "6", "5", "1", "8", "", "", "7", "3"},
+            {"3", "4", "7", "8", "5", "1", "9", "2", "6"},
+            {"6", "5", "8", "", "", "", "3", "1", "4"},
+            {"", "1", "", "3", "", "", "7", "5", "8"},
+            {"5", "2", "1", "9", "3", "8", "6", "4", "7"},
+            {"8", "9", "6", "", "", "7", "1", "3", ""},
+            {"4", "7", "3", "", "1", "", "8", "9", ""}
+        };
+        SudokuPuzzle puzzle = new SudokuPuzzle();
+        puzzle.setBoard(board);
+        List<String> digital = new ArrayList<>();
+        digital.add("4");
+        digital.add("6");
+
+        puzzle.setCandidate(5, 4, digital);
+        puzzle.setCandidate(5, 5, new ArrayList<>(digital));
+
+        Optional<HintModel> result = new YWingStrategy().tryStrategy(puzzle);
+        Assert.assertTrue(result.isPresent());
+        HintModel hintModel = result.get();
+        Assert.assertTrue(hintModel.isCandidate());
+        CandidateModel candidateModel = hintModel.getCandidateModel();
+
+        Map<Position, List<String>> causeMap = candidateModel.getCauseMap();
+        Assert.assertEquals(3, causeMap.size());
+        List<String> strings = causeMap.get(new Position(7, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("2", strings.get(0));
+        Assert.assertEquals("4", strings.get(1));
+
+        strings = causeMap.get(new Position(8, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("2", strings.get(0));
+        Assert.assertEquals("6", strings.get(1));
+
+        strings = causeMap.get(new Position(5, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("4", strings.get(0));
+        Assert.assertEquals("6", strings.get(1));
+
+        Map<Position, List<String>> deleteMap = candidateModel.getDeleteMap();
+        Assert.assertEquals(1, deleteMap.size());
+        List<String> delDigital = deleteMap.get(new Position(5, 5));
+        Assert.assertEquals(1, delDigital.size());
+        Assert.assertEquals("6", delDigital.get(0));
+    }
+
+    @Test
+    public void test_box_left_down_first() {
+        String[][] board = new String[][] {
+            {"5", "2", "1", "9", "3", "8", "6", "4", "7"},
+            {"8", "9", "6", "", "", "7", "1", "3", ""},
+            {"4", "7", "3", "", "1", "", "8", "9", ""},
+            {"3", "4", "7", "8", "5", "1", "9", "2", "6"},
+            {"6", "5", "8", "", "", "", "3", "1", "4"},
+            {"", "1", "", "3", "", "", "7", "5", "8"},
+            {"1", "8", "4", "", "", "3", "5", "6", "9"},
+            {"7", "3", "", "", "", "5", "", "8", "1"},
+            {"", "6", "5", "1", "8", "", "", "7", "3"}
+        };
+        SudokuPuzzle puzzle = new SudokuPuzzle();
+        puzzle.setBoard(board);
+        List<String> digital = new ArrayList<>();
+        digital.add("4");
+        digital.add("6");
+
+        puzzle.setCandidate(5, 4, digital);
+        puzzle.setCandidate(5, 5, new ArrayList<>(digital));
+
+        Optional<HintModel> result = new YWingStrategy().tryStrategy(puzzle);
+        Assert.assertTrue(result.isPresent());
+        HintModel hintModel = result.get();
+        Assert.assertTrue(hintModel.isCandidate());
+        CandidateModel candidateModel = hintModel.getCandidateModel();
+
+        Map<Position, List<String>> causeMap = candidateModel.getCauseMap();
+        Assert.assertEquals(3, causeMap.size());
+        List<String> strings = causeMap.get(new Position(5, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("4", strings.get(0));
+        Assert.assertEquals("6", strings.get(1));
+
+        strings = causeMap.get(new Position(2, 5));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("2", strings.get(0));
+        Assert.assertEquals("6", strings.get(1));
+
+        strings = causeMap.get(new Position(1, 4));
+        Assert.assertEquals(2, strings.size());
+        Assert.assertEquals("2", strings.get(0));
+        Assert.assertEquals("4", strings.get(1));
+
+        Map<Position, List<String>> deleteMap = candidateModel.getDeleteMap();
+        Assert.assertEquals(1, deleteMap.size());
+        List<String> delDigital = deleteMap.get(new Position(5, 5));
+        Assert.assertEquals(1, delDigital.size());
+        Assert.assertEquals("6", delDigital.get(0));
+    }
+
+    @Test
+    public void test_box_left_up_second() {
+        //todo 测试box 第二列
+        //todo 测试box 横向
+    }
+
 }
 
